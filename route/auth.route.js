3c938beb276d566bffdc112f261a7d0ad8c262c5
sumitbadri2000 <sumitbadri2000@gmail.com>
2023-12-16 19:00:59 +0530

    add

--- route/auth.route.js
+++ route/auth.route.js
@@ -9,7 +9,7 @@ const AuthRouter = express.Router();
 AuthRouter.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
-  const user = await AuthModel.find({ email });
+  const user = await UserModel.find({ email });
 
   if (user.length === 0) {
     bcrypt.hash(password, 5, async (err, hash) => {
@@ -17,8 +17,9 @@ AuthRouter.post("/register", async (req, res) => {
         res.status(400).send({ msg: "Something Went Wrong" });
       } else {
         try {
-          const newUser = new AuthModel({
+          const newUser = new UserModel({
             name,
+
             email,
             password: hash,
           });
@@ -37,13 +38,16 @@ AuthRouter.post("/register", async (req, res) => {
 AuthRouter.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await AuthModel.find({ email });
+  const user = await UserModel.find({ email });
 
   if (user.length > 0) {
     bcrypt.compare(password, user[0].password, async (err, result) => {
       if (result) {
         try {
-          const token = jwt.sign({ userID: user[0]._id }, process.env.secret);
+          const token = jwt.sign(
+            { userID: user[0]._id },
+            process.env.userSecretKey
+          );
           res.status(200).send({ msg: "Login Suceessful", token: token });
         } catch (e) {
           res.status(400).send({ msg: "Wrong Credentials", err: e.message });
